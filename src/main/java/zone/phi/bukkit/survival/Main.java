143ad39acf30bb9b26466b604eff9aea93407dec
Naptie <naptie@qq.com>
2022-08-02 16:19:49 +0800

    BossBar Style Change

--- src/main/java/zone/phi/bukkit/survival/Main.java
+++ src/main/java/zone/phi/bukkit/survival/Main.java
@@ -41,11 +41,10 @@ public class Main extends JavaPlugin {
         ChatUtils.init();
         this.skullManager = new SkullManager();
         this.auaHelper = new AUAHelper(Objects.requireNonNull(getConfig().getString("aua.root")), getConfig().getString("aua.token"), this.auaConfig);
-        this.bar = Bukkit.createBossBar(ChatUtils.t("&a您正在 &b&lmc.phi.zone &a上游玩"), BarColor.GREEN, BarStyle.SOLID);
+        this.bar = Bukkit.createBossBar(ChatUtils.t("&a您正在 &b&lmc.phi.zone &a上游玩"), BarColor.WHITE, BarStyle.SOLID);
         for (Player player : Bukkit.getOnlinePlayers()) {
             this.bar.addPlayer(player);
         }
-//        setBarAnimation();
         registerListeners();
         registerCommands();
         this.logger.info("已启用 " + getDescription().getName() + " v" + getDescription().getVersion());
@@ -53,15 +52,15 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        this.logger.info("已禁用 " + getDescription().getName() + " v" + getDescription().getVersion());
         saveConfigs(this.auaHelper.getBindings());
         Bukkit.getScheduler().cancelTasks(this);
         this.bar.removeAll();
         this.bar = null;
         this.skullManager = null;
         this.auaHelper = null;
-        this.logger = null;
         instance = null;
+        this.logger.info("已禁用 " + getDescription().getName() + " v" + getDescription().getVersion());
+        this.logger = null;
     }
 
     private void registerConfigs() {
@@ -75,51 +74,6 @@ public class Main extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
         getServer().getPluginManager().registerEvents(new PlayerQuit(), this);
     }
-
-//    private void setBarAnimation() {
-//        BossBar bar = Main.getInstance().getBar();
-//        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> {
-//            try {
-//                for (BarColor color : BarColor.values()) {
-//                    TimeUnit.SECONDS.sleep(1);
-//                    bar.setColor(color);
-//                }
-//            } catch (InterruptedException e) {
-//                throw new RuntimeException(e);
-//            }
-//
-//        }, 0, 0);
-//        List<String> titles = List.of(
-//                "&f您&a正在 &b&lmc.phi.zone &a上游玩",
-//                "&a您&f正&a在 &b&lmc.phi.zone &a上游玩",
-//                "&a您正&f在&a &b&lmc.phi.zone &a上游玩",
-//                "&a您正在 &f&lm&b&lc.phi.zone &a上游玩",
-//                "&a您正在 &b&lm&f&lc&b&l.phi.zone &a上游玩",
-//                "&a您正在 &b&lmc&f&l.&b&lphi.zone &a上游玩",
-//                "&a您正在 &b&lmc.&f&lp&b&lhi.zone &a上游玩",
-//                "&a您正在 &b&lmc.p&f&lh&b&li.zone &a上游玩",
-//                "&a您正在 &b&lmc.ph&f&li&b&l.zone &a上游玩",
-//                "&a您正在 &b&lmc.phi&f&l.&b&lzone &a上游玩",
-//                "&a您正在 &b&lmc.phi.&f&lz&b&lone &a上游玩",
-//                "&a您正在 &b&lmc.phi.z&f&lo&b&lne &a上游玩",
-//                "&a您正在 &b&lmc.phi.zo&f&ln&b&le &a上游玩",
-//                "&a您正在 &b&lmc.phi.zon&f&le &a上游玩",
-//                "&a您正在 &b&lmc.phi.zone &f上&a游玩",
-//                "&a您正在 &b&lmc.phi.zone &a上&f游&a玩",
-//                "&a您正在 &b&lmc.phi.zone &a上游&f玩",
-//                "&a您正在 &b&lmc.phi.zone &a上游玩");
-//        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> {
-//            try {
-//                for (String title : titles) {
-//                    bar.setTitle(ChatUtils.t(title));
-//                    TimeUnit.MILLISECONDS.sleep(50);
-//                }
-//            } catch (InterruptedException e) {
-//                throw new RuntimeException(e);
-//            }
-//
-//        }, 0, 0);
-//    }
 
     private void saveConfigs(Map<UUID, Integer> bindings) {
         try {
